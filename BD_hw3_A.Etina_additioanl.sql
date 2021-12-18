4c8c56dda224c8a3b3b1527829fcde6b12e3d3f5
Nava Etin <ffire.girl@gmail.com>
2021-12-18 19:59:41 +0200

    updated file additional ex.

--- BD_hw3_A.Etina_additioanl.sql
+++ BD_hw3_A.Etina_additioanl.sql
@@ -1,17 +1,17 @@
+create table if not exists departnets (
+	deprtment_id serial primary key,
+	department_name varchar (60) not null unique
+)
+
 create table if not exists employees (
 	empoloyee_id serial primary key,
-	empolyee_name varchar (100) not null unique
+	empolyee_name varchar (100) not null unique,
+	departament_id references departaments (departament_id)
 )
 
 create table if not exists heads (
 	head_id serial primary key,
 	head_name varchar (100) not null unique,
-	employee_id int2 references employees (employee_id)
-)
-
-create table if not exists departnets (
-	deprtment_id serial primary key,
-	department_name varchar (60) not null unique,
-	head_id int2 references heads (head_id),
-	employee_id int2 references employees (employee_id)
+	employee_id int2 references employees (employee_id),
+	departament_id int2 references departaments (departament_id)
 )
