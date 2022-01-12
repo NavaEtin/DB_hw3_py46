0c72653bd9092cf1cef525424628a1d36ec82b97
Nava Etin <ffire.girl@gmail.com>
2022-01-12 21:09:35 +0200

    updated ER and SQL file

--- DB_hw3.2_A.Etina.sql
+++ DB_hw3.2_A.Etina.sql
@@ -6,23 +6,24 @@ create table if not exists singer (
 create table if not exists music_style (
 	music_style_id serial primary key,
 	music_style_name varchar(45) not null unique
-	)
-    
+	)	
+ 
 create table if not exists album (
     album_id serial primary key,
-    singer_id int2 references singer (singer_id),
     album_name varchar(70) not null unique,
     album_year int2 not null
 )
 
 create table if not exists SingerAlbum (
 	singer_id int2 references singer (singer_id),
-	album_id int2 references album (album_id)
+	album_id int2 references album (album_id),
+	constraint pk primary key (singer_id, album_id)
 )
 
 create table if not exists SingerStyle (
 	singer_id int2 references singer (singer_id),
-	music_style_id int2 references music_style (music_style_id)
+	music_style_id int2 references music_style (music_style_id),
+	constraint pk_1 primary key (singer_id, music_style_id)
 )
 
 create table if not exists track (
@@ -35,11 +36,11 @@ create table if not exists track (
 create table if not exists collection (
     collection_id serial primary key,
     collection_name varchar(80) not null unique,
-    collection_year int2,
-    track_id int2 references track (track_id)
+    collection_year int2
 )
 
-create table if not exists CollectionAlbum (
-	collection_id references collection (collection_id),
-	album_id references album (album_id)
+create table if not exists CollectionTrack (
+	collection_id int2 references collection (collection_id),
+	track_id int2 references track (track_id),
+	constraint pk_2 primary key (collection_id, track_id)
 )
